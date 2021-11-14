36aeb2d610e23a5a242c9964b9611bbd41b7d910
owang-587 <baiwen@ualberta.ca>
2021-11-13 21:56:54 -0700

    latest attempt in gradient

--- testing stuff/numpytest.py
+++ testing stuff/numpytest.py
@@ -7,10 +7,29 @@ from matplotlib import pyplot
 # load numpy library
 import numpy as np
 
+def generate_block():
+    x = np.ones((50, 50, 3))
+    x[:, :, 0:3] = np.random.uniform(0, 1, (3,))
+    plt.imshow(x)
+    plt.figure() 
+
+    y = np.ones((50, 50, 3))
+    y[:,:,0:3] = np.random.uniform(0, 1, (3,))
+    plt.imshow(y)
+
+    plt.figure()
+    c = np.linspace(0, 1, 50)[:, None, None]
+    gradient = x + (y - x) * c
+    plt.imshow(gradient)
+    return x, y, gradient
+
+
+
+"""
 #load image as pixel array
 im = image.imread('./images/rockylabels.png')
 
 #display array as image
 pyplot.imshow(im)
 pyplot.show()
-
+"""
