e6201480f498c2316457ecc22eca405d6377aebd
owang-587 <baiwen@ualberta.ca>
2021-11-13 23:18:13 -0700

    got python working

--- testing stuff/numpytest.py
+++ testing stuff/numpytest.py
@@ -1,15 +1,28 @@
 ## using numpy for generation of gradient image ##
 
-# load and display image with matplotlib
+# load libraries
 from matplotlib import image
 from matplotlib import pyplot
 import matplotlib.pyplot as plt
-
-# load numpy library
 import numpy as np
+from PIL import Image
 
-f = np.array([1,2,3,6,11,31], dtype=float)
-np.gradient(f)
+def get_gradient_2d(start, stop, width, height, is_horizontal):
+    if is_horizontal:
+        return np.tile(np.linspace(start, stop, width), (height, 1))
+    else:
+        return np.tile(np.linspace(start, stop, height), (width, 1)).T
+
+def get_gradient_3d(width, height, start_list, stop_list, is_horizontal_list):
+    result = np.zeros((height, width, len(start_list)), dtype=float)
+
+    for i, (start, stop, is_horizontal) in enumerate(zip(start_list, stop_list, is_horizontal_list)):
+        result[:, :, i] = get_gradient_2d(start, stop, width, height, is_horizontal)
+
+    return result
+
+array = get_gradient_3d(512, 256, (0, 0, 0), (255, 255, 255), (True, True, True))
+Image.fromarray(np.uint8(array)).save('./images/teehee.png', quality=95)
 
 
 
