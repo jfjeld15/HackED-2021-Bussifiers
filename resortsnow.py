d27631c0c8b5476228a812a839755330c05e267e
jfjeld15 <jfjeld@ualberta.ca>
2021-11-14 11:38:38 -0700

    dsfadsadsafdsafsf

--- resortsnow.py
+++ resortsnow.py
@@ -2,68 +2,54 @@ import tkinter
 from tkinter.constants import NW
 
 """ LABELS:
-wpop: Whistler yearly snowfall (https://www.whistler.com/weather/history/)
+wpop: Whistler yearly snowfall (https://www.onthesnow.com/british-columbia/whistler-blackcomb/historical-snowfall)
 SPpop: Sun Peaks
-SSpop: Silverstar
+SSpop: Silverstar (https://www.onthesnow.com/british-columbia/silver-star/ski-resort)
 Revpop: Revelstoke
 kickpop: Kicking Horse
 louisepop: Lake Louise
 SVpop: Sunshine Village
 fernpop: Fernie
 RMpop: Red Mountain
-BWpop: Big White
+BWpop: Big White (https://www.onthesnow.com/british-columbia/big-white/historical-snowfall)
 """
 def genLabels(root, yearS):
-    if yearS.get() == 2003:
-        wpop,SPpop,SSpop,Revpop,kickpop,louisepop,SVpop,fernpop,RMpop,BWpop = "975 cm","sun peaks (cm)","x","x","x","x","x","x","x","x"
-    elif yearS.get() == 2004:
-        wpop,SPpop,SSpop,Revpop,kickpop,louisepop,SVpop,fernpop,RMpop,BWpop = "685 cm","sun peaks (cm)","x","x","x","x","x","x","x","x"
-    elif yearS.get() == 2005:
-        wpop,SPpop,SSpop,Revpop,kickpop,louisepop,SVpop,fernpop,RMpop,BWpop = "1192 cm","x","x","x","x","x","x","x","x","x"
-    elif yearS.get() == 2006:
-        wpop,SPpop,SSpop,Revpop,kickpop,louisepop,SVpop,fernpop,RMpop,BWpop = "1416 cm","x","x","x","x","x","x","x","x","x"
-    elif yearS.get() == 2007:
-        wpop,SPpop,SSpop,Revpop,kickpop,louisepop,SVpop,fernpop,RMpop,BWpop = "1019 cm","x","x","x","x","x","x","x","x","x"
-    elif yearS.get() == 2008:
-        wpop,SPpop,SSpop,Revpop,kickpop,louisepop,SVpop,fernpop,RMpop,BWpop = "930 cm","x","x","x","x","x","x","x","x","x"
-    elif yearS.get() == 2009:
-        wpop,SPpop,SSpop,Revpop,kickpop,louisepop,SVpop,fernpop,RMpop,BWpop = "1494 cm","x","x","x","x","x","x","x","x","x"
-    elif yearS.get() == 2010:
-        wpop,SPpop,SSpop,Revpop,kickpop,louisepop,SVpop,fernpop,RMpop,BWpop = "1579 cm","x","x","x","x","x","x","x","x","x"
-    elif yearS.get() == 2011:
-        wpop,SPpop,SSpop,Revpop,kickpop,louisepop,SVpop,fernpop,RMpop,BWpop = "1389 cm","x","x","x","x","x","x","x","x","x"
+    if yearS.get() == 2011:
+        wpop,SPpop,SSpop,Revpop,kickpop,louisepop,SVpop,fernpop,RMpop,BWpop = "711","414","406","485","376","467","531","754","353","1379"
     elif yearS.get() == 2012:
-        wpop,SPpop,SSpop,Revpop,kickpop,louisepop,SVpop,fernpop,RMpop,BWpop = "1112 cm","x","x","x","x","x","x","x","x","x"
+        wpop,SPpop,SSpop,Revpop,kickpop,louisepop,SVpop,fernpop,RMpop,BWpop = "696","437","366","561","561","757","688","787","290","424"
     elif yearS.get() == 2013:
-        wpop,SPpop,SSpop,Revpop,kickpop,louisepop,SVpop,fernpop,RMpop,BWpop = "905 cm","x","x","x","x","x","x","x","x","x"
+        wpop,SPpop,SSpop,Revpop,kickpop,louisepop,SVpop,fernpop,RMpop,BWpop = "556","386","376","549","498","465","429","544","391","508"
     elif yearS.get() == 2014:
-        wpop,SPpop,SSpop,Revpop,kickpop,louisepop,SVpop,fernpop,RMpop,BWpop = "672 cm","x","x","x","x","x","x","x","x","x"
+        wpop,SPpop,SSpop,Revpop,kickpop,louisepop,SVpop,fernpop,RMpop,BWpop = "1105","545","643","724","518","338","574","688","620","927"
     elif yearS.get() == 2015:
-        wpop,SPpop,SSpop,Revpop,kickpop,louisepop,SVpop,fernpop,RMpop,BWpop = "1257 cm","x","x","x","x","x","x","x","x","x"
+        wpop,SPpop,SSpop,Revpop,kickpop,louisepop,SVpop,fernpop,RMpop,BWpop = "1283","932","630","638","569","544","759","960","335","810"
     elif yearS.get() == 2016:
-        wpop,SPpop,SSpop,Revpop,kickpop,louisepop,SVpop,fernpop,RMpop,BWpop = "1307 cm","x","x","x","x","x","x","x","x","x"
+        wpop,SPpop,SSpop,Revpop,kickpop,louisepop,SVpop,fernpop,RMpop,BWpop = "803","467","574","615","617","737","762","919","841","861"
     elif yearS.get() == 2017:
-        wpop,SPpop,SSpop,Revpop,kickpop,louisepop,SVpop,fernpop,RMpop,BWpop = "1239 cm","x","x","x","x","x","x","x","x","x"
+        wpop,SPpop,SSpop,Revpop,kickpop,louisepop,SVpop,fernpop,RMpop,BWpop = "950","277","432","470","480","505","442","701","348","579"
     elif yearS.get() == 2018:
-        wpop,SPpop,SSpop,Revpop,kickpop,louisepop,SVpop,fernpop,RMpop,BWpop = "957 cm","x","x","x","x","x","x","x","x","x"
-    whistler = tkinter.Label(root, text="Whistler total snowfall in {}:\n{}".format(yearS.get(),wpop), width=28, bg="red")
+        wpop,SPpop,SSpop,Revpop,kickpop,louisepop,SVpop,fernpop,RMpop,BWpop = "749","414","386","531","447","536","686","643","297","630"
+    elif yearS.get() == 2019:
+        wpop,SPpop,SSpop,Revpop,kickpop,louisepop,SVpop,fernpop,RMpop,BWpop = "714","178","203","551","384","445","754","523","312","404"
+    whistler = tkinter.Label(root, text="Whistler total snowfall in {}:\n{} cm".format(yearS.get(),wpop), width=28, bg="gray")
     whistler.place(x=833, y=66, anchor=NW)
-    sunP = tkinter.Label(root, text="Sun Peaks total snowfall in {}:\n{}".format(yearS.get(),SPpop), width=28, bg="blue")
+    sunP = tkinter.Label(root, text="Sun Peaks total snowfall in {}:\n{} cm".format(yearS.get(),SPpop), width=28, bg="gray")
     sunP.place(x=833, y=116, anchor=NW)
-    silv = tkinter.Label(root, text="SilverStar total snowfall in {}:\n{}".format(yearS.get(),SSpop), width=28, bg="red")
+    silv = tkinter.Label(root, text="SilverStar total snowfall in {}:\n{} cm".format(yearS.get(),SSpop), width=28, bg="gray")
     silv.place(x=833, y=166, anchor=NW)
-    revy = tkinter.Label(root, text="Revelstoke total snowfall in {}:\n{}".format(yearS.get(),Revpop), width=28, bg="blue")
+    revy = tkinter.Label(root, text="Revelstoke total snowfall in {}:\n{} cm".format(yearS.get(),Revpop), width=28, bg="gray")
     revy.place(x=833, y=216, anchor=NW)
-    kicky = tkinter.Label(root, text="Kicking Horse total snowfall in {}:\n{}".format(yearS.get(),kickpop), width=28, bg="red")
+    kicky = tkinter.Label(root, text="Kicking Horse total snowfall in {}:\n{} cm".format(yearS.get(),kickpop), width=28, bg="gray")
     kicky.place(x=833, y=266, anchor=NW)
-    louise = tkinter.Label(root, text="Lake Louise total snowfall in {}:\n{}".format(yearS.get(),louisepop), width=28, bg="red")
+    louise = tkinter.Label(root, text="Lake Louise total snowfall in {}:\n{} cm".format(yearS.get(),louisepop), width=28, bg="gray")
     louise.place(x=833, y=316, anchor=NW)
-    SV = tkinter.Label(root, text="Sunshine Village total snowfall in {}:\n{}".format(yearS.get(),SVpop), width=28, bg="red")
+    SV = tkinter.Label(root, text="Sunshine Village total snowfall in {}:\n{} cm".format(yearS.get(),SVpop), width=28, bg="gray")
     SV.place(x=833, y=366, anchor=NW)
-    fernie = tkinter.Label(root, text="Fernie total snowfall in {}:\n{}".format(yearS.get(),fernpop), width=28, bg="red")
+    fernie = tkinter.Label(root, text="Fernie total snowfall in {}:\n{} cm".format(yearS.get(),fernpop), width=28, bg="gray")
     fernie.place(x=833, y=416, anchor=NW)
-    RM = tkinter.Label(root, text="Red Mountain total snowfall in {}:\n{}".format(yearS.get(),RMpop), width=28, bg="red")
+    RM = tkinter.Label(root, text="Red Mountain total snowfall in {}:\n{} cm".format(yearS.get(),RMpop), width=28, bg="gray")
     RM.place(x=833, y=466, anchor=NW)
-    BW = tkinter.Label(root, text="Big White total snowfall in {}:\n{}".format(yearS.get(),BWpop), width=28, bg="red")
+    BW = tkinter.Label(root, text="Big White total snowfall in {}:\n{} cm".format(yearS.get(),BWpop), width=28, bg="gray")
     BW.place(x=833, y=516, anchor=NW)
     
--- windowbase.py
+++ windowbase.py
@@ -7,18 +7,18 @@ import resortsnow as rs
 def initWindow(root):  # Create base window
     def printyear():
         rs.genLabels(root, yearS)  # Uses module resortsnow to generate labels in window
-        root.after(100, printyear)  # Update the labels every 100ms
+        root.after(200, printyear)  # Update the labels every 100ms
         return
 
     root.title("Bussifiers' Ski Trip Planner")
     earthmap = tk.PhotoImage(file="./images/rockylabels.png")
     baseC = tk.Canvas(root, width=1033, height=766)  # Canvas dimensions are the image dimensions
-    yearS = tk.Scale(root, from_=2003, to=2018, orient=HORIZONTAL, width=20, length=200)  # Slider for yearly snowfall data
+    yearS = tk.Scale(root, from_=2011, to=2019, orient=HORIZONTAL, width=20, length=200)  # Slider for yearly snowfall data
     baseC.create_image(0, 0, anchor=NW, image=earthmap)
     rb.genButts(root, baseC)  # Uses module resortbuttons to generate buttons that link resort websites
     yearS.place(x=833, y=0, anchor=NW)
     baseC.pack()
-    root.after(100, printyear)
+    root.after(200, printyear)
     root.mainloop()
 
 if __name__== "__main__":  # Main function
