bb617c710933a7044e7943dfe94c51d74cc17f2a
owang-587 <baiwen@ualberta.ca>
2021-11-13 18:02:56 -0700

    update1

--- window base.py
+++ window base.py
@@ -7,8 +7,8 @@ root = tk.Tk()
 canvas = tk.Canvas(root, height = 600, width = 600)
 canvas.pack()
 
-frame = tk.Frame(root, bg="#3e646c")
-frame.place(relwidth=0.8, relheight=0.8, rely=0.1, relx=0.1)
+earthmap = tk.Frame(root, bg="#3e646c")
+earthmap.place(relwidth=0.8, relheight=0.8, rely=0.1, relx=0.1)
 
 root.mainloop()
 
