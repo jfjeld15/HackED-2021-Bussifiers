45c0a45dd796fb7af9fb7d03f21f8660dbe750e1
owang-587 <baiwen@ualberta.ca>
2021-11-13 21:28:28 -0700

    latest quest to gradient picture

--- testing stuff/numpytest.py
+++ testing stuff/numpytest.py
@@ -1,8 +1,11 @@
-## using numpy for generation of gradient image##
+## using numpy for generation of gradient image ##
 
 # load and display image with matplotlib
 from matplotlib import image
 from matplotlib import pyplot
+
+# load numpy library
+import numpy as np
 
 #load image as pixel array
 im = image.imread('./images/rockylabels.png')
