dd2c3a6e4d33efe2cfdb4ba892c51bc7d1df5ab7
owang-587 <baiwen@ualberta.ca>
2021-11-14 01:49:15 -0700

    grad work 1:49am

--- numpytest.py
+++ numpytest.py
@@ -5,20 +5,21 @@ import matplotlib.pyplot as plt
 import numpy as np  # in windows terminal, run "pip install matplotlib"
 
 # initialize grid
-x_axis = np.linspace(-1, 1, 256)
-y_axis = np.linspace(-1, 1, 256)
+x_axis = np.linspace(-1, 1, 833)
+y_axis = np.linspace(-1, 1, 766)
 
 # generate np.array
 xx, yy = np.meshgrid(x_axis, y_axis)
 arr = np.sqrt(xx ** 2 + yy ** 2)
 
-# toggle color
-inner = np.array([0.1, 0.3, 0.4])[None, None, :]
-outer = np.array([0.9, 1, 0.9])[None, None, :]
+# adj color
+inner = np.array([0.1, 0.3, 0.3])[None, None, :]
+outer = np.array([0.8, 0.9, 0.8])[None, None, :]
 
-# control radius and radius color
+# adj radius size
 arr = arr[:, :, None]
-arr = (arr - 0.2) * outer + (1 - arr) * (inner)
+arr[0] = (arr+0.8) * outer + (arr) * (inner+0.4)
 
-plt.imshow(arr, cmap='gray')
+# disp gradient 
+plt.imshow(arr[:], cmap='gray')
 plt.show()
