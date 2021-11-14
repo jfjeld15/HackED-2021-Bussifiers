8a8dea936f2d51a17da5b7ef10b1245a84fb529e
jfjeld15 <jfjeld@ualberta.ca>
2021-11-13 21:51:22 -0700

    updates 9:50

--- resortbuttons.py
+++ resortbuttons.py
@@ -1,2 +1,12 @@
-def genButts(baseC):  # Generate Buttons
+import tkinter
+from tkinter.constants import CENTER
+import webbrowser
+
+def genButts(root, baseC):  # Generate Buttons
+    whistler = tkinter.Button(root, bg="black", command=open_whistler)
     baseC.create_window(105,498, width=11,height=9, anchor=CENTER, window=whistler)
+    return
+
+def open_whistler():
+    webbrowser.open_new_tab("https://www.whistlerblackcomb.com/")
+    return
