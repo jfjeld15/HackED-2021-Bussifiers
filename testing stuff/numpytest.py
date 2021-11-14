8cd00ce2b75bd27f4ef94bb8d08bc85aa0b7bfa3
owang-587 <baiwen@ualberta.ca>
2021-11-13 21:04:30 -0700

    successful import of im as array

--- testing stuff/numpytest.py
+++ testing stuff/numpytest.py
@@ -1,4 +1,13 @@
-# gradient generation from numpy
+## using numpy for generation of gradient image##
 
-import numpy as np
+# load and display image with matplotlib
+from matplotlib import image
+from matplotlib import pyplot
+
+#load image as pixel array
+im = image.imread('./images/rockylabels.png')
+
+#display array as image
+pyplot.imshow(im)
+pyplot.show()
 
