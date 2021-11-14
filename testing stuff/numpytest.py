53e92feb4dd60fd90520e3b251b7879d904cb007
owang-587 <baiwen@ualberta.ca>
2021-11-13 22:48:34 -0700

    losing hope

--- testing stuff/numpytest.py
+++ testing stuff/numpytest.py
@@ -3,25 +3,13 @@
 # load and display image with matplotlib
 from matplotlib import image
 from matplotlib import pyplot
+import matplotlib.pyplot as plt
 
 # load numpy library
 import numpy as np
 
-def generate_block():
-    x = np.ones((50, 50, 3))
-    x[:, :, 0:3] = np.random.uniform(0, 1, (3,))
-    plt.imshow(x)
-    plt.figure() 
-
-    y = np.ones((50, 50, 3))
-    y[:,:,0:3] = np.random.uniform(0, 1, (3,))
-    plt.imshow(y)
-
-    plt.figure()
-    c = np.linspace(0, 1, 50)[:, None, None]
-    gradient = x + (y - x) * c
-    plt.imshow(gradient)
-    return x, y, gradient
+f = np.array([1,2,3,6,11,31], dtype=float)
+np.gradient(f)
 
 
 
