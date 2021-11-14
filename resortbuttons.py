d14ceedd098a0a05dc30f43929e8278364e7baf8
jfjeld15 <jfjeld@ualberta.ca>
2021-11-13 23:19:02 -0700

    links

--- resortbuttons.py
+++ resortbuttons.py
@@ -2,11 +2,51 @@ import tkinter
 from tkinter.constants import CENTER
 import webbrowser
 
-def genButts(root, baseC):  # Generate Buttons
-    whistler = tkinter.Button(root, bg="black", command=open_whistler)
-    baseC.create_window(105,498, width=11,height=9, anchor=CENTER, window=whistler)
+def genButts(root, baseC, marker):  # Generate Buttons
+    whistler = tkinter.Button(root, bg="gold", command=open_whistler)  # Whistler
+    baseC.create_window(105,498, anchor=CENTER, window=whistler)
+
+    sunPeak = tkinter.Button(root, bg="gold", command=open_sunPeak)  # Sun Peaks
+    baseC.create_window(362,404, height=13, width=13, anchor=CENTER, window=sunPeak)
     return
 
-def open_whistler():
+def open_whistler():  # Whistler
     webbrowser.open_new_tab("https://www.whistlerblackcomb.com/")
     return
+
+def open_sunPeak():  # Sun Peaks
+    webbrowser.open_new_tab("https://www.sunpeaksresort.com/")
+    return
+
+def open_kicky():  # Kicking Horse
+    webbrowser.open_new_tab("https://kickinghorseresort.com/")
+    return
+
+def open_louise():  # Lake Louise
+    webbrowser.open_new_tab("https://www.skilouise.com/")
+    return
+
+def open_revy():  # Revelstoke
+    webbrowser.open_new_tab("https://www.revelstokemountainresort.com/")
+    return
+
+def open_sun():  # Sunshine
+    webbrowser.open_new_tab("https://www.skibanff.com/")
+    return
+
+def open_sStar():  # SilverStar
+    webbrowser.open_new_tab("https://www.skisilverstar.com/")
+    return
+
+def open_bWhite():  # Big White
+    webbrowser.open_new_tab("https://www.bigwhite.com/")
+    return
+
+def open_redMtn():  # Red Mtn
+    webbrowser.open_new_tab("https://www.redresort.com/")
+    return
+
+def open_fernie():  # Fernie
+    webbrowser.open_new_tab("https://skifernie.com/")
+    return
+
--- windowbase.py
+++ windowbase.py
@@ -1,14 +1,17 @@
 import tkinter as tk
-from tkinter.constants import NW, CENTER
+from tkinter.constants import NW
 import resortbuttons as rb
 
 
 def initWindow(root):  # Create base window
     root.title("Bussifiers' Ski Trip Planner")
+
     earthmap = tk.PhotoImage(file="./images/rockylabels.png")
+    marker = tk.PhotoImage(file="./images/marker.png")
+
     baseC = tk.Canvas(root, width=833, height=766)  # Canvas dimensions are the image dimensions
     baseC.create_image(0, 0, anchor=NW, image=earthmap)
-    rb.genButts(root, baseC)  # Uses module resortbuttons to generate buttons that link resort websites
+    rb.genButts(root, baseC, marker)  # Uses module resortbuttons to generate buttons that link resort websites
     baseC.pack()
     root.mainloop()
     return
