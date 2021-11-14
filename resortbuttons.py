b463a53bd83c8834d94eb12c394ffe25318fc791
jfjeld15 <jfjeld@ualberta.ca>
2021-11-13 23:37:39 -0700

    BUTTONS !

--- resortbuttons.py
+++ resortbuttons.py
@@ -2,12 +2,36 @@ import tkinter
 from tkinter.constants import CENTER
 import webbrowser
 
-def genButts(root, baseC, marker):  # Generate Buttons
+def genButts(root, baseC):  # Generate Buttons
     whistler = tkinter.Button(root, bg="gold", command=open_whistler)  # Whistler
-    baseC.create_window(105,498, anchor=CENTER, window=whistler)
+    baseC.create_window(105,498, height=13, width=13, anchor=CENTER, window=whistler)
 
     sunPeak = tkinter.Button(root, bg="gold", command=open_sunPeak)  # Sun Peaks
     baseC.create_window(362,404, height=13, width=13, anchor=CENTER, window=sunPeak)
+
+    kicky = tkinter.Button(root, bg="gold", command=open_kicky)  # Kicking Horse
+    baseC.create_window(599,345, height=13, width=13, anchor=CENTER, window=kicky)
+
+    louise = tkinter.Button(root, bg="gold", command=open_louise)  # Lake Louise
+    baseC.create_window(671,324, height=13, width=13, anchor=CENTER, window=louise)
+
+    revy = tkinter.Button(root, bg="gold", command=open_revy)  # Revelstoke
+    baseC.create_window(508,393, height=13, width=13, anchor=CENTER, window=revy)
+
+    sunsh = tkinter.Button(root, bg="gold", command=open_sunsh)  # Sunshine
+    baseC.create_window(706,365, height=13, width=13, anchor=CENTER, window=sunsh)
+
+    sStar = tkinter.Button(root, bg="gold", command=open_sStar)  # SilverStar
+    baseC.create_window(433,473, height=13, width=13, anchor=CENTER, window=sStar)
+
+    bWhite = tkinter.Button(root, bg="gold", command=open_bWhite)  # Big White
+    baseC.create_window(445,556, height=13, width=13, anchor=CENTER, window=bWhite)
+
+    redMtn = tkinter.Button(root, bg="gold", command=open_redMtn)  # Red Mtn
+    baseC.create_window(541,636, height=13, width=13, anchor=CENTER, window=redMtn)
+
+    fernie = tkinter.Button(root, bg="gold", command=open_fernie)  # Fernie
+    baseC.create_window(773,579, height=13, width=13, anchor=CENTER, window=fernie)
     return
 
 def open_whistler():  # Whistler
@@ -30,7 +54,7 @@ def open_revy():  # Revelstoke
     webbrowser.open_new_tab("https://www.revelstokemountainresort.com/")
     return
 
-def open_sun():  # Sunshine
+def open_sunsh():  # Sunshine
     webbrowser.open_new_tab("https://www.skibanff.com/")
     return
 
--- windowbase.py
+++ windowbase.py
@@ -7,11 +7,10 @@ def initWindow(root):  # Create base window
     root.title("Bussifiers' Ski Trip Planner")
 
     earthmap = tk.PhotoImage(file="./images/rockylabels.png")
-    marker = tk.PhotoImage(file="./images/marker.png")
 
     baseC = tk.Canvas(root, width=833, height=766)  # Canvas dimensions are the image dimensions
     baseC.create_image(0, 0, anchor=NW, image=earthmap)
-    rb.genButts(root, baseC, marker)  # Uses module resortbuttons to generate buttons that link resort websites
+    rb.genButts(root, baseC)  # Uses module resortbuttons to generate buttons that link resort websites
     baseC.pack()
     root.mainloop()
     return
